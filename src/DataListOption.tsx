6ce73d916cbbb598c4bc8a0b9a6b4a5a60eb6486
Utilisateur <franck.ledoux.pro@gmail.com>
2022-12-24 18:38:19 +0100

    add scss

--- src/DataListOption.tsx
+++ src/DataListOption.tsx
@@ -1,7 +1,10 @@
 import React, { FC, ReactNode } from 'react'
 
+import style from './dataListOption.module.scss'
+
 interface DataLOptionProps {
   children?: ReactNode
 }
 
-export const DataListOption: FC<DataLOptionProps> = ({ children }) => (<option>{children}</option>)
+export const DataListOption: FC<DataLOptionProps> = ({ children }) => (<option
+  className={style.dataListOption}>{children}</option>)
